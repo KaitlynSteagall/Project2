babce064d456d6c04c11fd65e55ccf18863739f3
Your Name <renee.petit@gmail.com>
2019-10-26 14:02:39 -0700

    dummy data entered

--- models/schema.sql
+++ models/schema.sql
@@ -1,17 +1,16 @@
-DROP DATABASE IF EXISTS puffinpowerDB;
 CREATE DATABASE puffinpowerDB;
 
-USE DATABASE puffinpowerDB;
+USE puffinpowerDB;
 
 CREATE TABLE users (
 	userIndex INT AUTO_INCREMENT NOT NULL,
     userName VARCHAR(100) NOT NULL,
     passwordName VARCHAR(100) NOT NULL,
     accessLevel INT NOT NULL,
-	primary key(index),
+	primary key(userIndex)
 );
 
-USE DATABASE puffinpowerDB;
+USE puffinpowerDB;
 
 CREATE TABLE puffins (
 	puffinIndex INT AUTO_INCREMENT NOT NULL,
@@ -19,27 +18,27 @@ CREATE TABLE puffins (
     gender VARCHAR(100),
     age INT,
     photos VARCHAR(10000),
-	primary key(index)
+	primary key(puffinIndex)
 );
 
-USE DATABASE puffinpowerDB;
+USE puffinpowerDB;
 
 CREATE TABLE public (
 	publicIndex INT AUTO_INCREMENT NOT NULL,
     publicName VARCHAR(100),
-    comments VARCHAR(100),
+    comments TEXT,
     photos VARCHAR(2048),
-	primary key(index)
+	primary key(publicIndex)
 );
 
-USE DATABASE puffinpowerDB;
+USE puffinpowerDB;
 
 CREATE TABLE notes (
 	notesIndex INT AUTO_INCREMENT NOT NULL,
     puffinName VARCHAR(100) NOT NULL,
     userName VARCHAR(100) NOT NULL,
     notes TEXT NOT NULL,
-	primary key(index),
+	primary key (notesIndex),
     FOREIGN KEY (puffinName) REFERENCES puffins(puffinName),
     FOREIGN KEY (userName) REFERENCES users(userName)
 );
