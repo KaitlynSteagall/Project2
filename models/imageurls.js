95ea0207d8bd9e0060856932d4bc235e6918ec4e
Your Name <renee.petit@gmail.com>
2019-10-31 20:12:21 -0700

    Added in Sequelize for models

---
+++ models/imageurls.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+    var Imageurls = sequelize.define("Imageurls", {
+      imgIndex: {
+          Type: DataTypes.INTEGER,
+          autoIncrement: true,
+          allowNull: false
+      },
+      imgurl: {
+          Type: DataTypes.TEXT,
+          allowNull: false
+      },
+      artistName: {
+          Type: DataTypes.STRING,
+          allowNull: false
+      },
+      puffinIndex: {
+          Type: DataTypes.INTEGER,
+          allowNull: false
+      }
+    });
+
+    Imageurls.associate = function(models) {
+        // We're saying that a Post should belong to an Author
+        // A Post can't be created without an Author due to the foreign key constraint
+        Imageurls.belongsTo(models.Puffins, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+    return Imageurls;
+  };
---
+++ models/puffins.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+    var Puffins = sequelize.define("Puffins", {
+      puffinIndex: {
+          Type: DataTypes.INTEGER,
+          autoIncrement: true,
+          allowNull: false
+      },
+      puffinName: {
+          Type: DataTypes.STRING,
+          allowNull: true
+      },
+      gender: {
+          Type: DataTypes.STRING,
+          allowNull: false
+      },
+      age: {
+          Type: DataTypes.INTEGER,
+          allowNull: false
+      }
+    });
+
+    Puffins.associate = function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        Puffins.hasMany(models.Notes, {
+          onDelete: "cascade"
+        });
+        Puffins.hasMany(models.Imageurls, {
+            onDelete: "cascade"
+        });
+      };
+    return Puffins;
+  };
